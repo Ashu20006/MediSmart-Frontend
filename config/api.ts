f2579b686b549c68aa554b207065e79c5258c7ca
Ashutosh Kumar <ag8449362@gmail.com>
2025-11-05 15:28:33 +0530

    change production

--- config/api.ts
+++ config/api.ts
@@ -1,4 +1,4 @@
-// const API_BASE_URL = "http://localhost:8080"; // change to your production URL later
-const API_BASE_URL = "https://medismart-backend-production.up.railway.app"; // change to your production URL later
+const API_BASE_URL = "http://localhost:8080"; // change to your production URL later
+// const API_BASE_URL = "https://medismart-backend-production.up.railway.app"; 
 
 export default API_BASE_URL;
